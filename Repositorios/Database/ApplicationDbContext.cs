b9ed485655dc6d263ef31a03a0e22885a8c54bf6
Juan Eduardo Calderón Servín <juanixgh@gmail.com>
2020-09-11 13:27:31 -0500

    Code first migrations funcionando

---
+++ Repositorios/Database/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Contratos.Modelos;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Repositorios.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private static string _dbConnection => Properties.Settings.Default.AspiriaConnection;
+        public ApplicationDbContext()
+            : base(_dbConnection)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
---
+++ Repositorios/Migrations/Configuration.cs
@@ -0,0 +1,20 @@
+﻿namespace Repositorios.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Database.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Database.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
